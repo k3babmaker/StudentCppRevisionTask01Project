eacea128675ebbcff0b6817fd0207f39231aba16
Kostya Kozlovsky <monodygames@gmail.com>
2023-03-09 17:45:08 +0300

    change solving of task02

--- StudentCppRevisionTask01Project/task02.cpp
+++ StudentCppRevisionTask01Project/task02.cpp
@@ -27,13 +27,15 @@
 */
 
 int task02(int a, int b) {
-	if (a > b) {
-		return 1;
-	}
-	else if (b > a) {
-		return 2;
-	}
-	else {
-		return 0;
-	}
+	//if (a > b) {
+	//	return 1;
+	//}
+	//else if (b > a) {
+	//	return 2;
+	//}
+	//else {
+	//	return 0;
+	//}
+
+	return a > b ? 1 : (a == b ? 0 : 2);
 }
